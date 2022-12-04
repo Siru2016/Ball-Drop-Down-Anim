d2f19e2c1eb0c14b68147da813068617ad7a9468
siraj <sirajudheentk@gmail.com>
2022-12-04 18:14:47 +0530

    Bouncing ball updated

--- app/src/main/java/com/anaad/demo/MainActivity.kt
+++ app/src/main/java/com/anaad/demo/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anaad.demo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration.get
+import android.view.animation.Animation
+import android.view.animation.BounceInterpolator
+import android.view.animation.TranslateAnimation
+import android.widget.EditText
 import androidx.core.view.isVisible
+import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity : AppCompatActivity() {
 
     private var view: View? = null
+    private var editText: TextInputEditText? = null
     private var diamond: View? = null
     private var dX = 0f
     private var dY:Float = 0f
@@ -20,11 +30,68 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        editText = findViewById(R.id.editText)
         view = findViewById(R.id.view)
         diamond = findViewById(R.id.circle)
-        diamond?.setOnTouchListener { view, motionEvent ->
-            onTouch(view, motionEvent)
+//        diamond?.setOnTouchListener { view, motionEvent ->
+//            onTouch(view, motionEvent)
+//        }
+
+        editText?.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                editText?.setLineSpacing(2f,2f)
+            }
+
+        })
+
+        diamond?.setOnClickListener {
+            view?.clearAnimation()
+            view?.isVisible = true
+
+            val array = arrayOf(78,156,234,312,390,468,546,624)
+            val transAnim = TranslateAnimation(
+                0f,0f,0f, (getTextLineHeight()).toFloat()
+            )
+            transAnim.startOffset = 500
+            transAnim.duration = 3000
+            transAnim.fillAfter = true
+            transAnim.interpolator = BounceInterpolator()
+            transAnim.setAnimationListener(object : Animation.AnimationListener{
+                override fun onAnimationStart(p0: Animation?) {}
+
+                override fun onAnimationEnd(p0: Animation?) {
+                    view?.clearAnimation()
+                    val left = view?.left
+                    val top = view?.top
+                    val right = view?.right
+                    val bottom = view?.bottom
+                    view?.layout(left!!, top!!, right!!,bottom!!)
+                }
+                override fun onAnimationRepeat(p0: Animation?) {} })
+
+            view?.startAnimation(transAnim)
         }
+    }
+
+    private fun displayHeight(): Int {
+        val displayMetrics = DisplayMetrics()
+        val windowManager = windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+    private fun getTextLineHeight(): Int {
+        editText?.setLineSpacing(2f,2f)
+        val lineCount = editText?.lineCount
+        val lineHeight = editText?.lineHeight
+        val height = editText?.measuredHeight
+        val totalLineHeight = lineCount!! * lineHeight!!
+        var result = 0
+
+        return totalLineHeight
     }
 
     private fun onTouch(p0: View?, event: MotionEvent?): Boolean {
